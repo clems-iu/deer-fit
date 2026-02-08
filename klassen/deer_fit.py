a9a5ce1109fa30bded0f9b5d27d0216fca591595
Clemens <clemens.vorspohl@iu-study.org>
2026-02-08 13:51:32 +0100

    Abänderung der Initialisierung über Dateien und Einbindung einer Speicherfunktion

--- klassen/deer_fit.py
+++ klassen/deer_fit.py
@@ -1,6 +1,7 @@
 from klassen.mitglieder import Mitglied
 from klassen.kurse import Kurs
 import json
+import os
 
 # Ankerpunkt für die DeerFit Anwendung
 
@@ -24,24 +25,47 @@ class DeerFit:
         for kurs in self.kurse:
             print(kurs)
 
-    #Lädt aus json Dateien initiale Daten für Mitglieder, Kurse, Bilanz, Equipment und Trainer
-    def lade_initialdaten(self, mitgliederpfad, kursepfad, bilanzpfad, trainerpfad, equipmentpfad):
+    def lade_initialdaten(self):
+        """Lädt initiale Daten aus JSON Dateien. Sucht zuerst in /initial/saves/, dann in /initial/basic/"""
+        dateien = ['mitglieder.json', 'kurse.json']
+        # Eigentlich, aber noch nicht voll entwickelt: dateien = ['mitglieder.json', 'kurse.json', 'bilanz.json', 'trainer.json', 'equipment.json']
+        pfade = {}
         
-
-        with open(mitgliederpfad, 'r') as f:
-            mitglieder_daten = json.load(f)
-            for md in mitglieder_daten:
+        for datei in dateien:
+            basis_pfad = os.path.dirname(__file__)
+            pfad_saves = os.path.join(basis_pfad, '..', 'initial', 'saves', datei)
+            pfad_basic = os.path.join(basis_pfad, '..', 'initial', 'basic', datei)
+            
+            try:
+                with open(pfad_saves, 'r') as f:
+                    pfade[datei] = pfad_saves
+            except FileNotFoundError:
+                try:
+                    with open(pfad_basic, 'r') as f:
+                        pfade[datei] = pfad_basic
+                except FileNotFoundError:
+                    raise FileNotFoundError(f"Datei {datei} nicht gefunden in saves/ oder basic/")
+        
+        with open(pfade['mitglieder.json'], 'r') as f:
+            for md in json.load(f):
                 mitglied = Mitglied(md['vorname'], md['nachname'], md['mitgliedsnummer'], md['trainingsfortschritt'], md['mitgliedschaft'])
                 self.mitglieder.append(mitglied)
-
-        with open(kursepfad, 'r') as f:
-            kurse_daten = json.load(f)
-            for kd in kurse_daten:
+        
+        with open(pfade['kurse.json'], 'r') as f:
+            for kd in json.load(f):
                 kurs = Kurs(kd['name'], kd['beschreibung'], kd['dauer'], kd['max_teilnehmer'], kd['offene_termine'])
                 self.kurse.append(kurs)
+        
+        #with open(pfade['bilanz.json'], 'r') as f:
+        #    self.bilanz = json.load(f)
 
-        with open(bilanzpfad, 'r') as f:
-            self.bilanz = json.load(f)
-
-        # Trainer und Equipment können hier ebenfalls geladen werden
-        # Implementierung abhängig von noch zu entwerfenden Klassen und Datenstrukturen
+    def speichere_daten(self):
+        """Speichert den aktuellen Stand der Daten in /initial/saves/"""
+        with open('initial/saves/mitglieder.json', 'w') as f:
+            json.dump([vars(m) for m in self.mitglieder], f, indent=2)
+        
+        with open('initial/saves/kurse.json', 'w') as f:
+            json.dump([vars(k) for k in self.kurse], f, indent=2)
+        
+        #with open('initial/saves/bilanz.json', 'w') as f:
+        #    json.dump(self.bilanz, f, indent=2)
