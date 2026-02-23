b42bef3885d832554bdf3667b8ebc2fedc382576
Clemens <clemens.vorspohl@iu-study.org>
2026-02-23 13:12:44 +0100

    Attributanpassungen für bessere Kompatibilität mit Speicherstruktur

--- app/klassen/kurse.py
+++ app/klassen/kurse.py
@@ -1,12 +1,12 @@
 #Enthält die Klassen für Kurse, Kurstermine Kursbuchungen und Kurspläne im Deer-Fit System.
 
 class Kurs:
-    def __init__(self, name, beschreibung, dauer, max_teilnehmer=10, offene_termine=[]):
+    def __init__(self, id, name, beschreibung, dauer, max_teilnehmer=10):
+        self.id = id
         self.name = name
         self.beschreibung = beschreibung
         self.dauer = dauer  # Dauer in Minuten
         self.max_teilnehmer = max_teilnehmer  # Maximale Teilnehmeranzahl
-        self.offene_termine = offene_termine  # Liste der Kurstermine
         
     def __str__(self):
         return f"{self.name}: {self.beschreibung} ({self.dauer} Minuten)"
@@ -16,29 +16,25 @@ class Kurstermin:
         self.kurs = kurs
         self.datum = datum  # Datum des Kurstermins
         self.uhrzeit = uhrzeit  # Uhrzeit des Kurstermins
-        self.kursbuchungen = []  # Liste der angemeldeten Mitglieder
+        self.kursbuchungen = []  # Liste der angemeldeten Mitglieder als Mitgliedsnummern
 
-    def teilnehmer_hinzufuegen(self, buchung):
+    def teilnehmer_hinzufuegen(self, mitgliedsnummer):
         if len(self.kursbuchungen) < self.kurs.max_teilnehmer:
-            self.kursbuchungen.append(buchung)
+            self.kursbuchungen.append(mitgliedsnummer)
         else:
             print("Kurs ist voll!")
 
-    def teilnehmer_entfernen(self, buchung):
-        self.kursbuchungen.remove(buchung)
+    def teilnehmer_entfernen(self, mitgliedsnummer):
+        if mitgliedsnummer in self.kursbuchungen:
+            self.kursbuchungen.remove(mitgliedsnummer)
 
     def __str__(self):
         return f"{self.kurs.name} am {self.datum} um {self.uhrzeit} mit {len(self.kursbuchungen)}/{self.kurs.max_teilnehmer} Teilnehmern"
 
         
-class Kursbuchung:
-    def __init__(self, mitglied, kurs, datum):
-        self.mitglied = mitglied
-        self.kurs = kurs
-        self.datum = datum  # Datum der Kursbuchung
 
     def __str__(self):
-        return f"{self.mitglied} hat sich für {self.kurs} am {self.datum} angemeldet."
+        return f"{self.mitgliedsnummer} hat sich für {self.kurs} am {self.datum} angemeldet."
     
 class Kursplan:
     def __init__(self, start, end, kurstermine=[]):
