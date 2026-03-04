22db623bbfa6aaaa4412da19fbe3ce3130381a08
Clemens <clemens.vorspohl@iu-study.org>
2026-03-04 13:38:16 +0100

    Neugestaltung des Login-Formulars

--- app/gui/login.py
+++ app/gui/login.py
@@ -2,21 +2,26 @@ import streamlit as st
 import app.klassen.authenticator as auth
 
 def show_login():
-	st.title("🦌 Deer-Fit Login")
-	mitgliedsnummer = st.text_input("Mitgliedsnummer")
-	password = st.text_input("Passwort", type="password")
-	if st.button("Login"):
-     
-		if mitgliedsnummer and password:
-			authenticator = auth.Authenticator()
-			authenticator.login(mitgliedsnummer, password)
-			if authenticator.authenticated:
-				st.session_state.logged_in = True
-				st.session_state.role = authenticator.role
-				st.session_state.mitgliedsnummer = authenticator.mitgliedsnummer
-				st.success(f"Erfolgreich als {authenticator.role} eingeloggt!")
-			else:
-				st.error("Ungültige Anmeldedaten. Bitte erneut versuchen.")
 
-		else:
-			st.warning("Bitte Mitgliedsnummer und Passwort eingeben.")
+	# mittlere von 3 Spalten nutzen (1/3 der Fläche)
+	col_left, col_center, col_right = st.columns([1, 1, 1])
+	with col_center:
+		with st.container():
+			st.title("🦌 Deer-Fit Login")
+			mitgliedsnummer = st.text_input("Mitgliedsnummer")
+			password = st.text_input("Passwort", type="password")
+
+			if st.button("Login"):
+				if mitgliedsnummer and password:
+					authenticator = auth.Authenticator()
+					authenticator.login(mitgliedsnummer, password)
+					if authenticator.authenticated:
+						st.session_state.logged_in = True
+						st.session_state.role = authenticator.role
+						st.session_state.mitgliedsnummer = authenticator.mitgliedsnummer
+						st.success(f"Erfolgreich als {authenticator.role} eingeloggt!")
+						st.rerun()
+					else:
+						st.error("Ungültige Anmeldedaten. Bitte erneut versuchen.")
+				else:
+					st.warning("Bitte Mitgliedsnummer und Passwort eingeben.")
