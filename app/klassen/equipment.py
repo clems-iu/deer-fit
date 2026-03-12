8dff083bf12b36a6f815d3b9aab3e6edc1e2db1d
Clemens <clemens.vorspohl@iu-study.org>
2026-03-12 11:22:52 +0100

    Einbindung von Attribut anschaffungsdatum statt anschaffungsjahr

--- app/klassen/equipment.py
+++ app/klassen/equipment.py
@@ -3,13 +3,13 @@
 import uuid
 
 class Equipment:
-	def __init__(self, name, anschaffungsjahr, kosten, sindKostenWiederkehrend=False):
+	def __init__(self, name, anschaffungsdatum, kosten, sindKostenWiederkehrend=False):
 		self.id = str(uuid.uuid4())  # Generiert eine eindeutige ID für jedes Equipment
 		self.name = name
-		self.anschaffungsjahr = anschaffungsjahr
+		self.anschaffungsdatum = anschaffungsdatum
 		self.kosten = kosten
 		self.sindKostenWiederkehrend = sindKostenWiederkehrend  # Standardmäßig auf False gesetzt
 		
 
 	def __str__(self):
-		return f"Equipment: {self.name} (Anschaffung: {self.anschaffungsjahr}, Kosten: {self.kosten} EUR, Wiederkehrend: {self.sindKostenWiederkehrend})"
+		return f"Equipment: {self.name} (Anschaffung: {self.anschaffungsdatum}, Kosten: {self.kosten} EUR, Wiederkehrend: {self.sindKostenWiederkehrend})"
